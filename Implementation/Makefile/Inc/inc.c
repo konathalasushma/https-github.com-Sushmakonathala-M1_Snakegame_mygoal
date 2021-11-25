b4cedbd367f60f8c8b97f3a05ba9b2d637593175
konathalasushma <94597753+konathalasushma@users.noreply.github.com>
2021-11-24 21:46:10 -0800

    Add files via upload

--- Implementation/Makefile/Inc/inc.c
+++ Implementation/Makefile/Inc/inc.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include<time.h>
+#include<ctype.h>
+#include <time.h>
+#include <windows.h>
+#include <process.h>
 
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
